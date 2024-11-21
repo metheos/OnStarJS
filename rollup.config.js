aa1d106c99ec71c38f6cd51bd2a9aa7e4cebbd13
Matt Nelson <metheos@gmail.com>
2024-11-21 14:58:29 -0600

    Add Rollup plugins for CommonJS and Node resolution
    This commit introduces the `@rollup/plugin-commonjs` and
    `@rollup/plugin-node-resolve` plugins to the Rollup configuration.
    These plugins enhance the build process by allowing the use of
    CommonJS modules and resolving Node.js modules, respectively.
    
    Additionally, the `package.json` and `pnpm-lock.yaml` files have
    been updated to include these new dependencies, ensuring that
    the project can leverage these features in its build pipeline.
    
    Signed-off-by: Matt Nelson <metheos@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json" with { type: "json" };
+import resolve from "@rollup/plugin-node-resolve";
+import commonJS from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/index.ts",
-  plugins: [json(), typescript({ tsconfig: "./tsconfig.json" })],
+  plugins: [
+    json(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    resolve(),
+    commonJS({
+      include: "node_modules/**",
+    }),
+  ],
   output: [
     { file: pkg.main, format: "cjs", exports: "default" },
     { file: pkg.module, format: "esm", exports: "default" },
