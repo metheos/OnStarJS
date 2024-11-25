ee77c5b46acb347edccb63af23a24b2c1cdefc73
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2024-11-25 16:52:22 -0600

    Update test config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  coveragePathIgnorePatterns: [
+    "./src/auth/GMAuth.ts", // Add the path to the file you want to exclude
+  ],
 };
