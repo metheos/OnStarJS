7c508c22e9ab989b74c27d26b6499538e5b8ede7
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2025-07-22 12:56:56 -0500

    feat(tests): adjust test timeout settings for improved reliability (#400)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  testTimeout: 90000, // Set global timeout to accommodate auth tests
   coveragePathIgnorePatterns: [
     "./src/auth/GMAuth.ts", // Add the path to the file you want to exclude
   ],
